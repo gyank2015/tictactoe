1d1140ee2e774f9173893ea2a761af6637a25073
gyank2015 <31079194+gyank2015@users.noreply.github.com>
2017-08-31 22:12:53 +0530

    Update tic_tac_toe.cpp

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-char square[10] = {'o','1','2','3','4','5','6','7','8','9'};
+char pos[10] = {'o','1','2','3','4','5','6','7','8','9'};
 
 int checkwin();
 void board();
@@ -21,33 +21,8 @@ int main()
 
 		mark=(player == 1) ? 'X' : 'O';
 
-		if (choice == 1 && square[1] == '1')
-
-			square[1] = mark;
-		else if (choice == 2 && square[2] == '2')
-
-			square[2] = mark;
-		else if (choice == 3 && square[3] == '3')
-
-			square[3] = mark;
-		else if (choice == 4 && square[4] == '4')
-
-			square[4] = mark;
-		else if (choice == 5 && square[5] == '5')
-
-			square[5] = mark;
-		else if (choice == 6 && square[6] == '6')
-
-			square[6] = mark;
-		else if (choice == 7 && square[7] == '7')
-
-			square[7] = mark;
-		else if (choice == 8 && square[8] == '8')
-
-			square[8] = mark;
-		else if (choice == 9 && square[9] == '9')
-
-			square[9] = mark;
+		if((choice>=1&&choice<=9)&&pos[choice]==choice+48)
+			pos[choice]=mark;
 		else
 		{
 			cout<<"Invalid move ";
@@ -72,43 +47,36 @@ int main()
 	return 0;
 }
 
-/*********************************************
-
-	FUNCTION TO RETURN GAME STATUS
-	1 FOR GAME IS OVER WITH RESULT
-	-1 FOR GAME IS IN PROGRESS
-	O GAME IS OVER AND NO RESULT
-**********************************************/
 
 int checkwin()
 {
-	if (square[1] == square[2] && square[2] == square[3])
+	if (pos[1] == pos[2] && pos[2] == pos[3])
 
 		return 1;
-	else if (square[4] == square[5] && square[5] == square[6])
+	else if (pos[4] == pos[5] && pos[5] == pos[6])
 
 		return 1;
-	else if (square[7] == square[8] && square[8] == square[9])
+	else if (pos[7] == pos[8] && pos[8] == pos[9])
 
 		return 1;
-	else if (square[1] == square[4] && square[4] == square[7])
+	else if (pos[1] == pos[4] && pos[4] == pos[7])
 
 		return 1;
-	else if (square[2] == square[5] && square[5] == square[8])
+	else if (pos[2] == pos[5] && pos[5] == pos[8])
 
 		return 1;
-	else if (square[3] == square[6] && square[6] == square[9])
+	else if (pos[3] == pos[6] && pos[6] == pos[9])
 
 		return 1;
-	else if (square[1] == square[5] && square[5] == square[9])
+	else if (pos[1] == pos[5] && pos[5] == pos[9])
 
 		return 1;
-	else if (square[3] == square[5] && square[5] == square[7])
+	else if (pos[3] == pos[5] && pos[5] == pos[7])
 
 		return 1;
-	else if (square[1] != '1' && square[2] != '2' && square[3] != '3' 
-                    && square[4] != '4' && square[5] != '5' && square[6] != '6' 
-                  && square[7] != '7' && square[8] != '8' && square[9] != '9')
+	else if (pos[1] != '1' && pos[2] != '2' && pos[3] != '3' 
+                    && pos[4] != '4' && pos[5] != '5' && pos[6] != '6' 
+                  && pos[7] != '7' && pos[8] != '8' && pos[9] != '9')
 
 		return 0;
 	else
@@ -125,17 +93,17 @@ void board()
 	cout << endl;
 
 	cout << "     |     |     " << endl;
-	cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
+	cout << "  " << pos[1] << "  |  " << pos[2] << "  |  " << pos[3] << endl;
 
 	cout << "_____|_____|_____" << endl;
 	cout << "     |     |     " << endl;
 
-	cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
+	cout << "  " << pos[4] << "  |  " << pos[5] << "  |  " << pos[6] << endl;
 
 	cout << "_____|_____|_____" << endl;
 	cout << "     |     |     " << endl;
 
-	cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
+	cout << "  " << pos[7] << "  |  " << pos[8] << "  |  " << pos[9] << endl;
 
 	cout << "     |     |     " << endl << endl;
 }
